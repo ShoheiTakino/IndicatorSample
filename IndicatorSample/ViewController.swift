2155c7c460619bf2b86fdac8892be227ae40971d
ShoheiTakino <shohei0522penguin@gmail.com>
2023-06-10 21:54:32 +0900

    Initial Commit

---
+++ IndicatorSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IndicatorSample
+//
+//  Created by 滝野翔平 on 2023/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
